1e76e72c82c938d6687a389f0c21a8ef92a04c52
Mark Entingh <entingh@gmail.com>
2021-01-06 23:09:41 -0800

    added Icon to IVendorInfo

--- Info.cs
+++ Info.cs
@@ -7,6 +7,7 @@ namespace Saber.Vendors.ReplaceTemplate
         public string Key { get; set; } = "ReplaceTemplate";
         public string Name { get; set; } = "Replace Template";
         public string Description { get; set; } = "Allows administrators to replace the default template website with the current website. This can be useful for deploying custom Saber releases with a special template website installed.";
+        public string Icon { get; set; }
         public Version Version { get; set; } = "1.0.0.0";
     }
 }
